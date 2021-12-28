3173180ae8c84d88ea285f229a477aee23dbafaf
Michael L. Collard <collard@uakron.edu>
2021-12-28 12:03:36 -0500

    Initial commit

---
+++ rainfall.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<float> rainfall;
+    float t;
+    float m;
+    float n;
+    while (cin >> n) {
+        rainfall.push_back(n);
+    }
+    t = rainfall[0];
+    m = rainfall[0];
+    for (vector<float>::size_type i = 1; i < rainfall.size(); ++i) {
+        t += rainfall[i];
+        if (rainfall[i] > m)
+            m = rainfall[i];
+    }
+    cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << (t / rainfall.size()) << " hundreds of inches" << '\n';
+    cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n';
+}
